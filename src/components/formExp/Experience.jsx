0574f638b5abcb95ad13d268e30ea17ff62f11d1
MichaelLELU <lelu.michael@gmail.com>
2024-05-06 03:09:15 +0200

    feat: form-exp.json map to homepage and create a component experience with props and proptype and css

---
+++ src/components/formExp/Experience.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import propTypes from 'prop-types';
+import './expStyle.scss'
+
+export default function Expercience ({exp}) {
+//state + fonction pour cacher/afficher les information dans les Experciences avec le boutton 
+    const [showDetailEx, setShowDetailEx] = useState(false);
+    const handleToggleEx = () => setShowDetailEx(!showDetailEx);
+
+    return  (
+    <li className="listExp">
+        <span className='expTitle'><h3>{exp.poste}</h3>
+        <button onClick={handleToggleEx} className='buttonMoreExp'>+</button></span>
+        {showDetailEx && 
+            <div>
+                <h4>{exp.entreprise}</h4>
+                <p>{exp.periode}</p>
+                <p> {exp.description}</p>
+            </div>}
+    </li>
+    
+    );
+}
+
+Expercience.propTypes ={
+    exp: propTypes.shape({
+        poste: propTypes.string.isRequired,
+        entreprise: propTypes.string.isRequired,
+        periode: propTypes.string.isRequired,
+        description: propTypes.string.isRequired, 
+    }.isRequired)}
