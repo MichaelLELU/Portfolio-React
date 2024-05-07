6d5291b82f8e853cd143339f60c086361ee00c4b
MichaelLELU <lelu.michael@gmail.com>
2024-05-07 18:21:11 +0200

    feat contact page and contact component with css responsive

--- src/pages/contactpage/ContactPage.jsx
+++ src/pages/contactpage/ContactPage.jsx
@@ -1,7 +1,14 @@
+import ContactForm from "../../components/contactForm/ContactForm";
+import './contactPageStyle.scss'
+
 export default function ContactPage () {
     return (
         <main>
-        <h1>Contact Page</h1>
+            <div className="top">
+                <h1>Contact</h1>
+                <p>Prêt à concrétiser vos idées ?<br/> Contactez-moi dès maintenant pour commencer à transformer vos idées en réalité digitale.</p>
+            </div>
+            <ContactForm/>
         </main>
     )
 }
