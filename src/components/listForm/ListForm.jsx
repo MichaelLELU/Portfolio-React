0b856365c18ee3770ad4ad7fd2a332a7f8a6cdbf
MichaelLELU <lelu.michael@gmail.com>
2024-05-22 02:49:49 +0200

    feat: projet page workin and add swiper to projects

--- src/components/listForm/ListForm.jsx
+++ src/components/listForm/ListForm.jsx
@@ -1,26 +1,35 @@
+import { useState } from "react";
 import Experience from '../../components/formExp/Experience';
 import Formation from '../../components/formExp/Formation';
 import formExp from '../../../public/data/formation-exp.json'
 import './listStyle.scss'
 
 export default function ListForm () {
+    const [showDetailForm, setShowDetailForm] = useState(false);
+    const handleToggleForm = () => setShowDetailForm(!showDetailForm);
+    const [showDetailExp, setShowDetailExp] = useState(false);
+    const handleToggleExp = () => setShowDetailExp(!showDetailExp);
 
     return (
         <div className='articles'>
         <article className='boxFormExp'>
-            <h2 className='titleH'>Formations:</h2>
-            <ul>
+            <span className="more"><h2 className='titleH'>Formations:</h2>
+            <button onClick={handleToggleForm} className='buttonMore'>{ showDetailForm? "-" : "+" } </button></span>
+            {showDetailForm && <ul>
                 {formExp.formation.map ((f)=> (
                     <Formation key={f.index} form={f}/>
                 )) }
-            </ul>
+            </ul>}
         </article>
+
         <article className='boxFormExp'>
-               <h2 className='titleH'>Expériences:</h2>
-                <ul> {formExp.experience.map((e) => (
+               <span className="more"><h2 className='titleH'>Expériences:</h2>
+               <button onClick={handleToggleExp} className='buttonMore'>{ showDetailExp? "-" : "+" } </button></span>
+               {showDetailExp && <ul> 
+                    {formExp.experience.map((e) => (
                 <Experience key={e.index} exp={e} handleToggleEx/>
                ))}
-                </ul>
+                </ul>}  
         </article>
         </div>
     )
--- src/pages/pageProjet/PageProjet.jsx
+++ src/pages/pageProjet/PageProjet.jsx
@@ -1,13 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import projects from '/public/data/projects.json';
 
 
 export default function PageProjet () {
     const { id } = useParams();
+    const projet = projects[id -1]
 
+    const handleBack = () => {
+        window.history.back();
+      };
 
     return (
-    <>
-    <h1>hello from {id} {id.name}</h1>
-    </>
+    <div>
+    <button type="button" onClick={handleBack}>Return</button>
+    <h1>{projet.nom}</h1>
+    <p>{projet.type}</p>
+    <img src={projet.image} alt={projet.alt} />
+    <p>{projet.description === "" ? projet.desc : projet.description}</p>
+    <Link to={projet.lienProd} target="_blank">SITE</Link>
+    <Link to={projet.lienCode} target="_blank">repos Github</Link>
+    <img src={projet.logo} alt={projet.logoAlt} />
+    </div>
     )
 }
