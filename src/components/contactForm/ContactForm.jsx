4926abe9227ba52273efc925923ef3390f01c6e9
MichaelLELU <lelu.michael@gmail.com>
2024-06-04 19:16:01 +0200

    feat: form Api and fix message

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,30 +1,36 @@
-import { useState } from 'react';
+
+import { useForm, ValidationError } from '@formspree/react';
 import './contactStyle.scss';
+import { Link } from 'react-router-dom';
 
 export default function ContactForm() {
 
-  const [submittedMessage, setSubmittedMessage] = useState('');
+  const [state, handleSubmit] = useForm("xvoejwad");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const submittedText = `Merci ${name}, une réponse vous sera apportée à l'adresse mail suivante : ${email} ! À bientôt!`;
-    setSubmittedMessage(submittedText);
-  };
+
+  if (state.succeeded) {
+
+    const submittedText = `Merci, une réponse vous sera apportée à vôtre adresse Mail ! À bientôt!`;
+    return (<div className='messageR'>
+        <Link type="button" to={"/"} className="buttonR">Retour</Link>
+        <p className='message'>{submittedText}</p>
+    </div>
+  )
+}
 
   return (
     <>
-      <form className="contactForm" onSubmit={handleSubmit} action="https://formspree.io/f/xvoejwad"
-  method="POST">
+      <form className="contactForm" onSubmit={handleSubmit} >
         <label htmlFor="name" className='formLabel'>Nom : </label>
         <input type="text" name="name" placeholder="Entrez votre nom" className='smallInput' required/>
+        <ValidationError prefix="Name" field="name" errors={state.errors}/>
         <label htmlFor="email" className='formLabel'>Email : </label>
         <input type="email" name="email" placeholder="votre@mail.com" className='smallInput' required/>
+        <ValidationError prefix="Email" field="email" errors={state.errors}/>
         <label htmlFor="message" className='formLabel'>Message : </label>
         <textarea type="text" name="message" placeholder="Écrivez votre message..." className='largeInput' />
-        <input type="submit" value="Envoi" className='buttonEnvoi'/>
-        {submittedMessage && <p className='message'>{submittedMessage}</p>}
+        <ValidationError prefix="Text" field="text" errors={state.errors}/>
+        <input type="submit" value="Envoi" className='buttonEnvoi' disabled={state.submitting}/>
       </form>
 
     </>
